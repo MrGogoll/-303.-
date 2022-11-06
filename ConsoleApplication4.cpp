f97654f786a18223f2bde998d83ba175def9e187
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-06 16:39:56 +0300

    Задача №303. Коровы

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int коров;
+    int n;
+    cin >> n;
+    if (n % 10 == 1) {
+        cout << n << " корова";
+    }
+    if (n % 10 == 2 || n % 10 == 3 || n % 10 == 4) {
+        cout << n << " коровы";
+    }
+    if (n % 10 != 2 && n % 10 != 3 && n % 10 != 4 && n % 10 != 1) {
+        cout << n << " коров";
+    }
+
+
+     
+}
+
